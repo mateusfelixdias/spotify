7aa129d1364e10aaaa3ab07da8165d6829d5803e
mateusfelixdias <felixmateus618@gmail.com>
2023-07-15 11:27:07 -0300

    feat: added sidebar

---
+++ app/components/Sidebar/index.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import Box from '../Box';
+import Library from '../Library';
+import SidebarItem from '../SidebarItem';
+
+import { useMemo } from 'react';
+import { usePathname } from 'next/navigation';
+
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+
+import IChildren from '@/app/interfaces/children';
+
+const Sidebar = ({ children }: IChildren) => {
+  const pathName = usePathname();
+
+  const isSearch = pathName === 'search';
+
+  const routes = useMemo(
+    () => [
+      { icon: HiHome, label: 'Home', active: !isSearch, href: '/' },
+      { icon: BiSearch, label: 'Pesquisar', active: isSearch, href: '/search' },
+    ],
+    [pathName]
+  );
+
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[18.75rem] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
+      </div>
+
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
