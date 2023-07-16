53eeb12980f5087ce338ff6d2b6bac6464340a19
mateusfelixdias <felixmateus618@gmail.com>
2023-07-16 09:43:21 -0300

    feat: added interface of the subscription

---
+++ interfaces/subscription/index.ts
@@ -0,0 +1,22 @@
+import Stripe from 'stripe';
+
+import IPrice from '../price';
+
+export default interface ISubscription {
+  id: string;
+  user_id: string;
+  created: string;
+  prices?: IPrice;
+  price_id: string;
+  quantity?: string;
+  ended_at?: string;
+  cancel_at?: string;
+  trial_end?: string;
+  canceled_at?: string;
+  trial_start?: string;
+  current_period_end: string;
+  metadata?: Stripe.Metadata;
+  current_period_start: string;
+  status?: Stripe.Subscription;
+  cancel_at_period_end?: boolean;
+}
