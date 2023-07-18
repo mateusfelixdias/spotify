84bebf0dac6b4ac310585b5d4bdca2bcb2ab0090
mateusfelixdias <felixmateus618@gmail.com>
2023-07-18 12:07:01 -0300

    feat: added play on music

--- components/PageContent/index.tsx
+++ components/PageContent/index.tsx
@@ -4,11 +4,15 @@ import SongItem from '../SongItem';
 
 import ISong from '@/interfaces/song';
 
+import useOnPlay from '@/hooks/useOnPlay';
+
 interface Props {
   songs: ISong[];
 }
 
 const PageContent = ({ songs }: Props) => {
+  const onPlay = useOnPlay(songs);
+
   if (!songs.length) {
     return <div>Não têm músicas disponíveis</div>;
   }
@@ -16,7 +20,11 @@ const PageContent = ({ songs }: Props) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
       {songs.map((song) => (
-        <SongItem song={song} key={song.id} onClick={(id: string) => {}} />
+        <SongItem
+          song={song}
+          key={song.id}
+          onClick={(id: string) => onPlay(id)}
+        />
       ))}
     </div>
   );
