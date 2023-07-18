011092cd558d421040fc9d6d329f89f4d36e797d
mateusfelixdias <felixmateus618@gmail.com>
2023-07-18 08:06:56 -0300

    feat: added song list

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,13 @@
+import getSongs from '@/actions/getSongs';
 import Header from '../../components/Header';
 import ListItem from '../../components/ListItem';
+import PageContent from '@/components/PageContent';
 
-export default function Home() {
+export const revalidade = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -12,7 +18,7 @@ export default function Home() {
 
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
             <ListItem
-              href="curtidas"
+              href="liked"
               name="Músicas Curtidas"
               image="/images/liked.webp"
             />
@@ -22,10 +28,12 @@ export default function Home() {
 
       <div className="mt-2 mb-7 px-6">
         <div className="flex justify-between items-center">
-          <h1 className="text-white text-2xl font-semibold">...</h1>
+          <h1 className="text-white text-2xl font-semibold">
+            Músicas mais novas
+          </h1>
         </div>
 
-        <div>...</div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
