2368f6ed084765861cbf1710f742a89e752a62ee
mateusfelixdias <felixmateus618@gmail.com>
2023-07-18 12:03:48 -0300

    feat: added player content

---
+++ components/PlayerContent/index.tsx
@@ -0,0 +1,143 @@
+import Slider from '../Slider';
+
+import useSound from 'use-sound';
+
+import ISong from '@/interfaces/song';
+
+import MediaItem from '../MediaItem';
+import LikeButton from '../LikeButton';
+
+import usePlayer from '@/hooks/usePlayer';
+
+import { useEffect, useState } from 'react';
+
+import { BsPlayFill, BsPauseFill } from 'react-icons/bs';
+import { HiSpeakerXMark, HiSpeakerWave } from 'react-icons/hi2';
+import { AiFillStepBackward, AiFillStepForward } from 'react-icons/ai';
+
+interface Props {
+  song: ISong;
+  songUrl: string;
+}
+
+const PlayerContent = ({ song, songUrl }: Props) => {
+  const player = usePlayer();
+
+  const [volume, setVolume] = useState(0.5);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  const onPlayNext = () => {
+    if (!player.ids.length) return;
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const nextSong = player.ids[currentIndex + 1];
+
+    if (!nextSong) {
+      player.setId(player.ids[0]);
+      return;
+    }
+
+    player.setId(nextSong);
+  };
+
+  const onPlayPrevious = () => {
+    if (!player.ids.length) return;
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const previousSong = player.ids[currentIndex - 1];
+
+    if (!previousSong) {
+      player.setId(player.ids[player.ids.length - 1]);
+      return;
+    }
+
+    player.setId(previousSong);
+  };
+
+  const handlePlay = () => {
+    if (!isPlaying) play();
+    else pause();
+  };
+
+  const toggleMute = () => {
+    if (!volume) setVolume(1);
+    else setVolume(0);
+  };
+
+  const [play, { pause, sound }] = useSound(songUrl, {
+    volume,
+    format: ['mp3'],
+    onplay: () => setIsPlaying(true),
+    onpause: () => setIsPlaying(false),
+    onend: () => {
+      setIsPlaying(false);
+      onPlayNext();
+    },
+  });
+
+  useEffect(() => {
+    sound?.play();
+
+    return () => {
+      sound?.unload();
+    };
+  }, [sound]);
+
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 h-full">
+      <div className="flex w-full justify-start">
+        <div className="flex items-center gap-x-4">
+          <MediaItem song={song} />
+          <LikeButton songId={song.id} />
+        </div>
+      </div>
+
+      <div className="flex md:hidden col-auto w-full justify-end items-center">
+        <div
+          onClick={handlePlay}
+          className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
+        >
+          <Icon size={30} className="text-black" />
+        </div>
+      </div>
+
+      <div className="hidden h-full md:flex justify-center items-center w-full max-w-[45.125rem] gap-x-6">
+        <AiFillStepBackward
+          size={30}
+          onClick={onPlayPrevious}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+
+        <div
+          onClick={handlePlay}
+          className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
+        >
+          <Icon size={300} className="text-black" />
+        </div>
+
+        <AiFillStepForward
+          size={30}
+          onClick={onPlayNext}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+      </div>
+
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[7.5rem]">
+          <VolumeIcon
+            size={34}
+            onClick={toggleMute}
+            className="cursor-pointer"
+          />
+
+          <Slider value={volume} onChange={(value) => setVolume(value)} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerContent;
